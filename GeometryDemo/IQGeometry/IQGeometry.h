8f741a9bf7b372f4ce32cdecfcca3b999974c7a3
Павел Литвиненко <gerasim13@gmail.com>
2014-09-02 23:04:35 +0400

    Fixed import directives on the iOS target

--- GeometryDemo/IQGeometry/IQGeometry.h
+++ GeometryDemo/IQGeometry/IQGeometry.h
@@ -8,10 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-#import <IQGeometry/IQGeometry+AffineTransform.h>
-#import <IQGeometry/IQGeometry+Angle.h>
-#import <IQGeometry/IQGeometry+Distance.h>
-#import <IQGeometry/IQGeometry+Line.h>
-#import <IQGeometry/IQGeometry+Point.h>
-#import <IQGeometry/IQGeometry+Rect.h>
-#import <IQGeometry/IQGeometry+Size.h>
+#if TARGET_OS_IPHONE
+    #import "IQGeometry+AffineTransform.h"
+    #import "IQGeometry+Angle.h"
+    #import "IQGeometry+Distance.h"
+    #import "IQGeometry+Line.h"
+    #import "IQGeometry+Point.h"
+    #import "IQGeometry+Rect.h"
+    #import "IQGeometry+Size.h"
+#else
+    #import <IQGeometry/IQGeometry+AffineTransform.h>
+    #import <IQGeometry/IQGeometry+Angle.h>
+    #import <IQGeometry/IQGeometry+Distance.h>
+    #import <IQGeometry/IQGeometry+Line.h>
+    #import <IQGeometry/IQGeometry+Point.h>
+    #import <IQGeometry/IQGeometry+Rect.h>
+    #import <IQGeometry/IQGeometry+Size.h>
+#endif
